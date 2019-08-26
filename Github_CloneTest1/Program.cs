6649b6eedf9c54fcf4fa546fa7f72a2c1ccaf81f
Kevin at MMS <x8neter@gmail.com>
2019-08-26 15:50:41 +1200

    Add project files.

---
+++ Github_CloneTest1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Github_CloneTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
